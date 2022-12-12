27aab395f384ee6ca8f2baa8786964a54f09f376
Bharanilikith <Bharanilikith@Bharani>
2022-12-12 11:15:48 +0530

    Hi

---
+++ src/scrollBar/Test0.java
@@ -0,0 +1,30 @@
+package scrollBar;
+
+import java.time.Duration;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test0 {
+
+	public static void main(String[] args) throws InterruptedException {
+
+WebDriver driver=new ChromeDriver();
+driver.manage().window().maximize();
+driver.get("https://www.amazon.com/");
+driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+//Typcasting
+JavascriptExecutor js=(JavascriptExecutor) driver;
+js.executeScript("window.scrollBy(0,5000)");
+
+Thread.sleep(3000);
+
+js.executeScript("window.scrollBy(0,-5000)");
+
+
+
+	}
+
+}
---
+++ src/scrollBar/Test1.java
@@ -0,0 +1,44 @@
+package scrollBar;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test1 {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.amazon.com/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+		
+		WebElement ele = driver.findElement(By.xpath("//img[@alt='New arrivals in Toys']"));
+
+	    Point p = ele.getLocation();
+	    int x = p.getX();
+	    int y=  p.getY();
+	    
+	    //typcasting
+	    JavascriptExecutor js=(JavascriptExecutor) driver;
+	    js.executeScript("window.scrollBy("+x+","+y+")");
+	    
+	    Thread.sleep(5000);
+	    
+	   // ele.click();
+	    
+	    //to perform click using javascriptexecutor
+//	    JavascriptExecutor js=(JavascriptExecutor) driver;
+//	    js.executeScript("arguments[0].click();",ele);
+//		
+	    
+		
+	}
+
+}
